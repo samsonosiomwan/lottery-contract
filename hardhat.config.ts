cf9ad0900673ae6a6d9d8b2ea6d93dc52b245f75
mac <samsonosiomwan@gmail.com>
2022-08-25 14:31:17 +0100

    added deployment script to testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config()
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+
+  networks:{
+    rinkeby:
+    {  url:`${process.env.ALCHEMY_URL}`,
+      accounts:[`${process.env.PRIVATE_KEY}`]}
+  }
 };
 
 export default config;
